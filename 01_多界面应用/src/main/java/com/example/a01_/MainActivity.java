26342fdf6ba530cb620f395f8ef4d360fe602d3e
a443934876 <378163883@qq.com>
2017-08-30 22:39:15 +0800

    update

--- 01_多界面应用/src/main/java/com/example/a01_/MainActivity.java
+++ 01_多界面应用/src/main/java/com/example/a01_/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.a01_;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt_show= (Button) findViewById(R.id.bt_show);
+        bt_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
